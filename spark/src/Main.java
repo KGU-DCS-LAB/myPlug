6b7666f9aabb3bef3efc1c920c83e7433df3dbdb
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-02-15 21:54:56 +0900

    spark를 위한 java 앱 푸쉬

---
+++ spark/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public void sayHello(){
+        System.out.println("Hello World");
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        main.sayHello();
+    }
+}
