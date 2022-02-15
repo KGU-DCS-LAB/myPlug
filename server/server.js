5b97fc5ff736737b6dc3ae557d13b7ee78c14607
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-02-15 23:49:50 +0900

    푸쉬 테스트

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.get('/', function(요청, 응답){
 
 
 
-// 1. mongoose 모듈 가져오기
+// 1. mongoose 모듈 가져오기!
 var mongoose = require('mongoose');
 // 2. testDB 세팅
 mongoose.connect('mongodb://localhost:27017/testDB');
