dca1156690055dc36ca652cc2c9014d8cfd6bd37
김가영 <83538598+gykim0923@users.noreply.github.com>
2022-02-21 01:03:48 +0900

    메인화면 구현

--- app-expo/App.js
+++ app-expo/App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Main from './src/routes/main/Main';
 
+
 function App() {
   return (
     <NavigationContainer>
