f826168b6b68214aee81fff591133a5944464b63
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-02-21 14:13:33 +0900

    Stack 구조로 다시 돌아옴!

--- app-expo/App.js
+++ app-expo/App.js
@@ -1,7 +1,5 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Main from './src/routes/main/Main';
 
 
--- app-expo/src/routes/main/Main.js
+++ app-expo/src/routes/main/Main.js
@@ -1,164 +1,78 @@
-import 'react-native-gesture-handler';
 import * as React from 'react';
-import { Button, View, Text, StyleSheet, Image } from 'react-native';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import HomeView from '../../views/main/HomeView';
-import { MaterialIcons } from '@expo/vector-icons'; 
-import { MaterialCommunityIcons } from '@expo/vector-icons'; 
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeView from  '../../views/main/HomeView';
+
 function HomeScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button onPress={() =>navigation.navigate('로딩화면')} title="그냥 잘 뜨는지 확인해보고 싶어서 만들어본 아무 의미 없는 버튼. 이 다음 HomeView는 왜 뜨지 않을까?" />
-      <HomeView/>
-    </View>
+    <HomeView/>
   );
 }
 
-function LoadingScreen({ navigation }) {
-  return (
-    <View  style={{ flex: 1, alignItems: 'center' }}>
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text style = {{fontWeight : 'bold', fontSize : 40}}>나만의{'\n'}플러그</Text>
-      <Text style = {{ fontSize : 20}}>전기차 충전앱</Text>
-      </View>
-      
-        <Text>충전소정보 업데이트 중 ...(50%)</Text>
-      
-     
-      <Button onPress={() => navigation.navigate('메인화면')} title="MyPlugHome화면으로 가기" />
-    </View>
-  );
-}
-
-function MainScreen({ navigation }) {
-  return (
-  <View  style={{ flex: 1, alignItems: 'center' }}>
-
-      <View style={{ flex: 1, alignItems: 'center', flexDirection: 'row' }}>
-          <MaterialIcons name="my-location" size={24} color="black" />
-         <Text>수원시 영통구 이의동 </Text>
-       </View>
-
-       <View style={{ flex: 1, flexDirection: 'row'}}>
-         
-               <View style={styles.CardContainer}>  
-               
-               
-               <MaterialCommunityIcons name="map-marker" size={40} color="black" >
-                 <Text style={styles.CardTitle}>충전소 지도</Text></MaterialCommunityIcons> 
-               </View>
-               <View style={styles.CardContainer}>  <MaterialIcons name="electric-car" size={40} color="black" >
-                 <Text style={styles.CardTitle}>충전 기록하기</Text></MaterialIcons> </View>
-              
-        </View> 
-
-
-        <View style={{ flex: 1, flexDirection: 'colume' }}>
-             <View style={styles.CardContainer}>  <Text style={styles.CardTitle}>인근 충전소 찾기</Text></View>
-           
-                  
-        </View> 
-
-
-        <View style={{ flex: 1, flexDirection: 'row'}}>
-        <View style={styles.CardContainer}>  <Text style={styles.CardTitle}>선호하는 충전소1</Text></View>
-        <View style={styles.CardContainer}>  <Text style={styles.CardTitle}>선호하는 충전소2</Text></View>
-              
-        </View> 
-
-        <View style={{ flex: 1, flexDirection: 'row'}}>
-        <View style={styles.CardContainer}>  <Text style={styles.CardTitle}>자유게시판</Text></View>
-        <View style={styles.CardContainer}>  <Text style={styles.CardTitle}>우리 동네 게시판</Text></View>
-              
-        </View> 
-
-
-        <View style={{ flex: 1, flexDirection: 'colume' }}>
-        <View style={styles.CardContainer}>  <Text style={styles.CardTitle}>인근장소추천</Text></View>
-        
-        </View> 
-
-
-        <View style={{ flex: 1, flexDirection: 'colume' }}>
-        <View style={styles.CardContainer}>  <Text style={styles.CardTitle}>개인 충전 패턴 분석</Text></View>
-        
-        </View>
-       
-
-        <View style={{ flex: 1, flexDirection: 'colume' }}>
-        <View style={styles.CardContainer}>  <Text style={styles.CardTitle}>차종별 충전 패턴 분석</Text></View>
-       
-                  
-        </View> 
-
-       <Button onPress={() => navigation.goBack()} title="Go back home" />
-
-    </View>
-  );
-}
-const styles = StyleSheet.create({
-  CardContainer: {
-      // elevation: 5,
-      // borderRadius: 7,
-      // borderWidth: 0.5,
-      // borderColor: '#black',
-      // margin: 20,
-      // padding: 10,
-      // flex: 1,
-      // width:150,
-      // height: 150
-      flex: 1,
-    backgroundColor: 'white',
-    borderRadius: 7,
-      borderWidth: 0.5,
-      borderColor: '#black',
-      margin: 5,
-      alignItems:'stretch'
-  },
-  CardTitle: {
-      width: '100%',
-      fontWeight: 'bold',
-      fontSize: 15,
-      padding: 3
-  },
-  CardContent: {
-      width: '100%',
-      fontSize: 12,
-      padding: 3
-  },
-});
-
-
-
-
-
-function NotificationsScreen({ navigation }) {
+function EvChargerScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
     </View>
   );
 }
-
-function SettingsScreen({ navigation }) {
+function CommunityScreen({ navigation }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+  );
+}
+function HotPlaceScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+      <Button
+        title="Go to Details... again"
+        onPress={() => navigation.push('Details')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
     </View>
   );
 }
 
-const Drawer = createDrawerNavigator();
+const Stack = createNativeStackNavigator();
 
-export default function Main() {
+function App() {
   return (
-    <Drawer.Navigator initialRouteName="Home">
-      <Drawer.Screen name="MyPlug" component={HomeScreen} />
-      <Drawer.Screen name="알림" component={NotificationsScreen} />
-      <Drawer.Screen name="설정" component={SettingsScreen} />
-      <Drawer.Screen name="로딩화면" component={LoadingScreen} />
-      <Drawer.Screen name="메인화면" component={MainScreen} />
-    </Drawer.Navigator>
+    <Stack.Navigator initialRouteName="Home">
+      <Stack.Screen name="Home" component={HomeScreen} />
+      {/* <Stack.Screen name="EvCharger" component={EvChargerScreen} />
+      <Stack.Screen name="Community" component={CommunityScreen} />
+      <Stack.Screen name="HotPlace" component={HotPlaceScreen} /> */}
+    </Stack.Navigator>
   );
 }
+
+export default App;
