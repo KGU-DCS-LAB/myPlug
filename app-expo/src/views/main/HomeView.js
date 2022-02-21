fd59cc0d6567477ad41a4fcd60687a3b3dd59995
김가영 <83538598+gykim0923@users.noreply.github.com>
2022-02-21 16:22:14 +0900

    MyPlug 메인화면 구현

--- app-expo/src/views/main/HomeView.js
+++ app-expo/src/views/main/HomeView.js
@@ -1,19 +1,144 @@
 import * as React from 'react';
-import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import 'react-native-gesture-handler';
+import { Button, View, Text, StyleSheet, Image } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons'; 
+import { MaterialCommunityIcons } from '@expo/vector-icons'; 
 
 function HomeView() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text>Home Screen!!!!!</Text>
-    <Button
-      title="Go to Details"
-      onPress={() => navigation.navigate('Details')}
-    />
-  </View>
-  );
+    <View  style={{ flex: 1, alignItems: 'center' }}>
+
+
+          <View style={{ flex: 1, alignItems: 'center', flexDirection: 'row' }}>
+          <MaterialIcons name="my-location" size={24} color="black" />
+        <Text>수원시 영통구 이의동 </Text>
+          </View> 
+
+
+          <View style={{ flex: 1, flexDirection: 'row'}}>
+                    
+                    <View style={styles.CardContainer}>  
+                    <MaterialCommunityIcons name="map-marker" size={40} color="black" > <Text style={styles.CardTitle}>충전소 지도</Text></MaterialCommunityIcons> 
+                      
+                    </View>
+                    
+                    
+                    <View style={styles.CardContainer}>  
+                    <MaterialIcons name="electric-car" size={40} color="black" > <Text style={styles.CardTitle}>충전 기록하기</Text></MaterialIcons>
+                      
+                    </View>
+
+              </View> 
+
+
+              <View style={{ flex: 1, flexDirection: 'column'}}>
+                    
+                    <View style={styles.CardContainer}>  
+                    <MaterialCommunityIcons name="map-marker" size={40} color="black" > <Text style={styles.CardTitle}>인근 충전소 찾기</Text></MaterialCommunityIcons> 
+                      
+                    </View>
+         
+              </View> 
+
+
+              <View style={{ flex: 1, flexDirection: 'row'}}>
+                    
+                    <View style={styles.CardContainer}>  
+                    <MaterialCommunityIcons name="map-marker" size={40} color="black" > <Text style={styles.CardTitle}>선호하는 충전소1</Text></MaterialCommunityIcons> 
+                      
+                    </View>
+                    
+                    
+                    <View style={styles.CardContainer}>  
+                    <MaterialIcons name="electric-car" size={40} color="black" > <Text style={styles.CardTitle}>선호하는 충전소2</Text></MaterialIcons>
+                      
+                    </View>
+
+              </View> 
+
+
+              
+              <View style={{ flex: 1, flexDirection: 'row'}}>
+                    
+                    <View style={styles.CardContainer}>  
+                    <MaterialCommunityIcons name="map-marker" size={40} color="black" > <Text style={styles.CardTitle}>자유게시판</Text></MaterialCommunityIcons> 
+                      
+                    </View>
+                    
+                    
+                    <View style={styles.CardContainer}>  
+                    <MaterialIcons name="electric-car" size={40} color="black" > <Text style={styles.CardTitle}>우리 동네 게시판</Text></MaterialIcons>
+                      
+                    </View>
+
+              </View> 
+
+
+              
+              <View style={{ flex: 1, flexDirection: 'column'}}>
+                    
+                    <View style={styles.CardContainer}>  
+                    <MaterialCommunityIcons name="map-marker" size={40} color="black" > <Text style={styles.CardTitle}>인근장소 추천</Text></MaterialCommunityIcons> 
+                      
+                    </View>
+         
+              </View> 
+
+
+              <View style={{ flex: 1, flexDirection: 'column'}}>
+                    
+                    <View style={styles.CardContainer}>  
+                    <MaterialCommunityIcons name="map-marker" size={40} color="black" > <Text style={styles.CardTitle}>개인 충전 패턴 분석</Text></MaterialCommunityIcons> 
+                      
+                    </View>
+         
+              </View> 
+
+
+              <View style={{ flex: 1, flexDirection: 'column'}}>
+                    
+                    <View style={styles.CardContainer}>  
+                    <MaterialCommunityIcons name="map-marker" size={40} color="black" > <Text style={styles.CardTitle}>차종별 충전 패턴 분석</Text></MaterialCommunityIcons> 
+                      
+                    </View>
+         
+              </View> 
+
+
+            
+
+
+ </View>
+ );
 }
+const styles = StyleSheet.create({
+  CardContainer: {
+
+      flex: 1,
+      elevation: 5,
+    backgroundColor: 'white',
+    borderRadius: 7,
+      borderWidth: 0.5,
+      borderColor: 'black',
+      margin: 5,
+      flexDirection: 'column',
+      alignItems: 'stretch'
+      
+  },
+  CardTitle: {
+      width: '100%',
+      fontWeight: 'bold',
+      fontSize: 15,
+      padding: 3
+  },
+  CardContent: {
+      width: '100%',
+      fontSize: 12,
+      padding: 3
+  },
+});
 
 
 export default HomeView;
