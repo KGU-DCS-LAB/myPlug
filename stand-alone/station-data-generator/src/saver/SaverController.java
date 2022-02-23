d8d0ec34265f6da9ec62a15bc391854ff4406171
kim-yeonsuu <yeonsu0329@kyonggi.ac.kr>
2022-02-23 16:56:45 +0900

    몽고db 연결 테스트

--- stand-alone/station-data-generator/src/saver/SaverController.java
+++ stand-alone/station-data-generator/src/saver/SaverController.java
@@ -13,40 +13,68 @@ import org.bson.Document;
  *
  * */
 public class SaverController {
-    public static void main(String args[]){
-        String MongoDB_IP = "127.0.0.1";
-        int MongoDB_PORT = 27017;
-        String DB_NAME = "testDB";
 
-        //Connect to MongoDB
-        MongoClient  mongoClient = new MongoClient(new ServerAddress(MongoDB_IP, MongoDB_PORT));
-//        DB db = mongoClient.getDB(DB_NAME);
-//        MongoCredential credential = MongoCredential.createCredential();
-        MongoDatabase db = mongoClient.getDatabase(DB_NAME);
-//        DBCollection collection = db.getCollection("schemas");
+    public static void main(String[] args) {
+        MongoClient mongo = new MongoClient("localhost", 27017);
+        MongoCredential credential = MongoCredential.createCredential("sampleUser","myDb","password".toCharArray());
+        System.out.println("Connected");
 
-//        MongoCollection<Document> collection = db.getCollection();
+        MongoDatabase database = mongo.getDatabase("myDb");
+        System.out.println("Collection created successfully");
 
+        System.out.println("Credentials : "+credential);
+        MongoCollection<Document> collection = database.getCollection("myCollection");
+        System.out.println("Collection myCollection selected successfully");
+//https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=cc116105&logNo=221479553429
+        Document document = new Document("title", "MongoDB")
+                .append("id", 1)
+                .append("description", "database")
+                .append("likes", 100)
+                .append("url", "http://www.tutorialspoint.com/mongodb/")
+                .append("by", "tutorials point");
+        collection.insertOne(document);
 
-//        //View Database List
-//        List<String> databases = mongoClient.getDatabaseNames();
-//
-//        System.out.println("=====Database List===== ");
-//        int num =1 ;
-//        for (String dbName : databases) {
-//            System.out.println( num  + ". " + dbName);
-//            num++;
-//        }
-//
-//        System.out.println();
-//
-//        //Connect Database and Show Collection List in Database
-//        DB db = mongoClient.getDB(DB_NAME);
-//        Set<String> collections = db.getCollectionNames();
-//
-//        System.out.println("Database : " + DB_NAME);
-//        for (String colName : collections) {
-//            System.out.println(" + Collection: " + colName);
-//        }
+        System.out.println("Document inserted successfully");
+
     }
+
+//    public static void main(String args[]){
+//
+//
+//
+//        String MongoDB_IP = "127.0.0.1";
+//        int MongoDB_PORT = 27017;
+//        String DB_NAME = "testDB";
+//
+//        //Connect to MongoDB
+//        MongoClient  mongoClient = new MongoClient(new ServerAddress(MongoDB_IP, MongoDB_PORT));
+////        DB db = mongoClient.getDB(DB_NAME);
+////        MongoCredential credential = MongoCredential.createCredential();
+//        MongoDatabase db = mongoClient.getDatabase(DB_NAME);
+////        DBCollection collection = db.getCollection("schemas");
+//
+////        MongoCollection<Document> collection = db.getCollection();
+//
+//
+////        //View Database List
+////        List<String> databases = mongoClient.getDatabaseNames();
+////
+////        System.out.println("=====Database List===== ");
+////        int num =1 ;
+////        for (String dbName : databases) {
+////            System.out.println( num  + ". " + dbName);
+////            num++;
+////        }
+////
+////        System.out.println();
+////
+////        //Connect Database and Show Collection List in Database
+////        DB db = mongoClient.getDB(DB_NAME);
+////        Set<String> collections = db.getCollectionNames();
+////
+////        System.out.println("Database : " + DB_NAME);
+////        for (String colName : collections) {
+////            System.out.println(" + Collection: " + colName);
+////        }
+//    }
     }
