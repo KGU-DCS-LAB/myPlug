23856c6af8ac311e9cb06e6e2531f9c19a44d61a
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-02-25 02:07:24 +0900

    [윤주현/ADD] 지도 관련 업데이트 시도중

--- app-expo/src/views/ev_chrager/EvChargerMapView.js
+++ app-expo/src/views/ev_chrager/EvChargerMapView.js
@@ -1,17 +1,70 @@
-import React from 'react'; 
-import MapView from 'react-native-maps';
-export default class EvChargerMapView extends React.Component { 
-  render() { 
+import React, { useState, useEffect } from 'react'; 
+import { StyleSheet } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import * as Location from 'expo-location';
+
+const EvChargerMapView = () => { 
+
+  const [initialRegion, setInitialRegion] = useState({
+    latitude: 35.91395373474155,
+    longitude: 127.73829440215488,
+    latitudeDelta: 5,
+    longitudeDelta: 5,
+  })
+  const [mapWidth, setMapWidth] = useState('99%');
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+
+  // Update map style to force a re-render to make sure the geolocation button appears
+  const updateMapStyle = () => {
+    setMapWidth('100%')
+  }
+
+  // Get current location information 
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+    console.log('[LOG] current location : ' + text);
+  }
+
+
+
     return ( 
-    <MapView 
-    style={{ flex: 1 }} 
-    initialRegion={{ 
-      latitude: 37.78825, 
-      longitude: -122.4324, 
-      latitudeDelta: 0.0922, 
-      longitudeDelta: 0.0421, 
-    }} 
-    /> 
+      <MapView
+        initialRegion={initialRegion}
+        style={[styles.map, { width: mapWidth }]}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation={true}
+        showsMyLocationButton={true}
+        onMapReady={() => {
+          updateMapStyle()
+        }}
+      />
     ); 
-  } 
 }
+
+export default EvChargerMapView;
+
+const styles = StyleSheet.create({
+  map: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+});
