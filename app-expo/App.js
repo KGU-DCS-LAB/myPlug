29b7a90c0d3aef79de6d84ab51b7ac86f783e599
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-03-09 20:50:30 +0900

    SafeAreaView 적용으로 상단바 영역 보호(안드로이드 및 아이폰 둘 다 지원)

--- app-expo/App.js
+++ app-expo/App.js
@@ -2,13 +2,24 @@ import React from 'react';
 import MainRoute from './src/routes/main/MainRoute';
 import { NativeBaseProvider } from 'native-base';
 import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet,StatusBar,SafeAreaView } from 'react-native';
 
 export default function App() {
   return (
-    <NativeBaseProvider>
-      <NavigationContainer>
-       <MainRoute/>
-      </NavigationContainer>
-    </NativeBaseProvider>
+    <SafeAreaView style={styles.AndroidSafeArea}>
+      <NativeBaseProvider>
+        <NavigationContainer>
+          <MainRoute/>
+        </NavigationContainer>
+      </NativeBaseProvider>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  AndroidSafeArea: {
+    flex: 1,
+    backgroundColor: "white",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  }
+});
