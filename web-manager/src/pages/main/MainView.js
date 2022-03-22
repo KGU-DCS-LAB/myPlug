2d451012cc7eb5c0a99c19f6310976a8ba0b76bb
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-03-23 00:04:43 +0900

    웹으로 몽고디비 관리를 할 수 있도록 기본 환경 구성 (프록시 설정 했음 확인필수)

--- web-manager/src/pages/main/MainView.js
+++ web-manager/src/pages/main/MainView.js
@@ -1,28 +1,66 @@
 // import logo from '../../logo.svg';
+import React, { useState, useEffect } from 'react';
 import '../../App.css';
-import { Link } from "react-router-dom";
 import Header from "../../components/common/Header"
+import axios from 'axios';
+
 // import { Button, Col, Row, } from 'react-bootstrap';
 
-
 function MainView() {
-    return (
-      <>
-      {/* <Row className="mx-0">
-        <Button as={Col} variant="primary">Button #1</Button>
-        <Button as={Col} variant="secondary" className="mx-2">Button #2</Button>
-        <Button as={Col} variant="success">Button #3</Button>
-      </Row> */}
-      <Header/>
-      <div className="container">
-        <div className="row my-4">
-        <div className="col-md-3 card">dd1</div>
-        <div className="col-md-9 card">dd2</div>
-        </div>
-      </div>
-      </>
-    );
+  const [loading, setLoading] = useState(true);
+  const [charging_stations, setChargingStation] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      // 충전소 데이터 받아오는 작업 시작
+      //ip는 변경되어야 할 가능성이 높으니깐 주의 바람 (윤주현)
+      axios.get('/stationsRouter/find')
+      .then((response) => {
+        setChargingStation(response.data);
+        // setChargingStation(JSON.stringify(response.data));
+        console.log(JSON.stringify(response.data))
+        setLoading(false);
+      }).catch(function (error) {
+        setChargingStation('serverError');
+        console.log(error);
+      });
+     // 충전소 데이터 받아오는 작업 끝
+    })();
+  }, []);
+
+  function ListItem(props) {
+    return <li>{props.value}</li>;
   }
   
-  export default MainView;
+  function StationList(props) {
+    const stations = props.stations;
+    return (
+      <ul>
+        {stations.map((station) =>
+          <ListItem key={station._id}
+                    value={station.charging_station_name} />
+        )}
+      </ul>
+    );
+  }
+  if (loading) return <div>Loading...</div>;
+  return (
+    <>
+    {/* <Row className="mx-0">
+      <Button as={Col} variant="primary">Button #1</Button>
+      <Button as={Col} variant="secondary" className="mx-2">Button #2</Button>
+      <Button as={Col} variant="success">Button #3</Button>
+    </Row> */}
+    <Header/>
+    <div className="container">
+      <div className="row my-4">
+        <div className="col-md-3 card">dd1</div>
+        <div className="col-md-9 card"><StationList stations={charging_stations} /></div>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default MainView;
   
