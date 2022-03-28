842df813ea5db9d2d5612b87dc53923d8f842448
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-03-28 16:16:18 +0900

    checked가 false인 데이터 검색 후,  true로 전환되도록 순서를 정해줌

--- stand-alone/data-manager/routes/Stations.js
+++ stand-alone/data-manager/routes/Stations.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router();
 const bodyParser = require('body-parser');
 const { RawStation } = require("../models/RawStation");
+const { Station } = require("../models/Station");
 
 /* GET. */
 router.get('/find/raw/all', function(req, res, next) {
@@ -16,16 +17,33 @@ router.get('/find/raw/all', function(req, res, next) {
     });
 });
 
-router.get('/update/raw/false', async (req, res) => {
+router.get('/update/raw/false', async (req, res, next) => {
         //수집한 원본 데이터 중 한번도 검사하지 않은 데이터 업데이트 하기
+
+        let updateStations = async () => {
+            RawStation.find({checked: {$eq: false}}).then( (stations) => {
+                // console.log(stations);
+                for (let i = 0; i < stations.length; i++) {
+                    let stationName = stations[i].charging_station_name;
+                    
+                    console.log(stationName);
+                }
+            }).catch( (err) => {
+                console.log(err);
+                next(err)
+            });    
+        }
+
+
         const filter = {checked:{$eq:false}};
         const updateDoc = {checked:true};
         let setStatus = (msg) =>{
             // console.log(msg)
             let text = msg.modifiedCount+'개의 새로운 수신 데이터가 최신화 되었습니다.';
+            console.log(text);
             res.json({status: text});
         }
-        let hello = async () => {
+        let updateCheckedFalseToTrue = async () => {
             RawStation.updateMany(filter, updateDoc,
                 (err, docs) => {
                     if (err){
@@ -37,7 +55,9 @@ router.get('/update/raw/false', async (req, res) => {
                 }
             );
         }
-        hello();
+
+        updateStations();
+        updateCheckedFalseToTrue();
 });
 
 router.get('/find/raw/false', function(req, res, next) {
