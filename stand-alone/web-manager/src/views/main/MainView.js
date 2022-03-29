a58b3bd76d62a095a30024a6740dad0747d310fd
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-03-29 11:20:13 +0900

    명령 카테고리를 아이콘으로 분류, 데이터 처리 후 뒤로가기 키 도입

--- stand-alone/web-manager/src/views/main/MainView.js
+++ stand-alone/web-manager/src/views/main/MainView.js
@@ -15,9 +15,9 @@ function MainView() {
             <h2>데이터 관리 시작하기</h2>
             <p>하단의 메뉴를 통해 데이터 관리를 할 수 있습니다.</p>
             <ul class="icon-list">
-              <li><a href="data/get_raw_all">수집한 모든 원본 데이터 확인하기</a></li>
-              <li><a href="data/get_raw_checked_false">수집한 전체 원본 데이터 중 checked가 false인 데이터 확인하기</a></li>
-              <li><a href="data/update_raw_all">수집한 원본 데이터 중 한번도 검사하지 않은 데이터 업데이트 하기</a></li>
+              <li><a href="data/get_raw_all">📄 수집한 모든 원본 데이터 확인하기</a></li>
+              <li><a href="data/get_raw_checked_false">📄 수집한 전체 원본 데이터 중 checked가 false인 데이터 확인하기</a></li>
+              <li><a href="data/update_raw_all">🔃 수집한 원본 데이터 중 한번도 검사하지 않은 데이터 업데이트 하기</a></li>
             </ul>
           </div>
     
