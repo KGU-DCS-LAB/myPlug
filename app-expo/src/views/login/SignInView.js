850ad249c0d1b2a5a6303ede457ece7545c759b5
soyoung125 <sypark041754@gmail.com>
2022-03-31 16:07:02 +0900

    회원가입 페이지 datePicker 추가

--- app-expo/src/views/login/SignInView.js
+++ app-expo/src/views/login/SignInView.js
@@ -1,6 +1,54 @@
 import { Center, Box, Heading, VStack, FormControl, Input, Pressable, Button, HStack, Text } from "native-base"
+import { useState } from "react";
+import DateTimePicker from '@react-native-community/datetimepicker';
+
+Date.prototype.format = function (f) {
+    if (!this.valueOf()) return " ";
+
+    const weekName = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"];
+    let d = this;
+
+    return f.replace(/(yyyy|yy|MM|dd|E|hh|mm|ss|a\/p)/gi, function ($1) {
+        switch ($1) {
+            case "yyyy": return d.getFullYear();
+            case "yy": return (d.getFullYear() % 1000).zf(2);
+            case "MM": return (d.getMonth() + 1).zf(2);
+            case "dd": return d.getDate().zf(2);
+            case "E": return weekName[d.getDay()];
+            case "HH": return d.getHours().zf(2);
+            case "hh": return ((h = d.getHours() % 12) ? h : 12).zf(2);
+            case "mm": return d.getMinutes().zf(2);
+            case "ss": return d.getSeconds().zf(2);
+            case "a/p": return d.getHours() < 12 ? "오전" : "오후";
+            default: return $1;
+        }
+    });
+}
+
+String.prototype.string = function (len) { var s = '', i = 0; while (i++ < len) { s += this; } return s; };
+String.prototype.zf = function (len) { return "0".string(len - this.length) + this; };
+Number.prototype.zf = function (len) { return this.toString().zf(len); };
 
 const SignInView = () => {
+    const [mode, setMode] = useState('date');
+    const [show, setShow] = useState(false);
+    const [BirthDay, setBirthDay] = useState(new Date());
+
+    const onChange = (event, selectedDate) => {
+        const currentDate = selectedDate;
+        setShow(false);
+        setBirthDay(currentDate);
+    };
+
+    const showMode = (currentMode) => {
+        setShow(true);
+        setMode(currentMode);
+    };
+
+    const showDatepicker = () => {
+        showMode('date');
+    }
+
     return (
         <Center w="100%" h="100%">
             <Box safeArea p="2" w="90%" maxW="290" py="8">
@@ -37,24 +85,26 @@ const SignInView = () => {
                     </FormControl>
                     <FormControl>
                         <FormControl.Label>생년월일</FormControl.Label>
-                        {/* <Pressable onPress={showDatePicker}>
+                        <Pressable onPress={showDatepicker}>
                             <Input
-                                value={UserBirthDay}
+                                value={BirthDay.format('yyyy-MM-dd')}
                                 returnKeyType="next"
                                 editable={false}
                                 blurOnSubmit={false}
                             />
-                            <DateTimePickerModal
-                                isVisible={isDatePickerVisible}
-                                headerTextIOS={'생년월일'}
-                                mode="date"
-                                onConfirm={handleConfirm}
-                                onCancel={hideDatePicker}
-                            />
-                        </Pressable> */}
+                            {show && (
+                                <DateTimePicker
+                                    testID="dateTimePicker"
+                                    value={BirthDay}
+                                    mode={mode}
+                                    is24Hour={true}
+                                    onChange={onChange}
+                                />
+                            )}
+                        </Pressable>
                     </FormControl>
                     <Button mt="2" colorScheme="indigo">
-                        Sign up
+                        회원가입
                     </Button>
                 </VStack>
             </Box>
