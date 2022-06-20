5bfd9e5cb84dea1fb3a4799a5a1fc37ed30af3af
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-06-21 01:24:38 +0900

    사용자가 이동할 때 지도가 사용자 위치를 따라오게 하는 기능 구현

--- app-expo/src/containers/ev_charger/EvChargerContainer.js
+++ app-expo/src/containers/ev_charger/EvChargerContainer.js
@@ -3,45 +3,78 @@ import { View } from "react-native";
 import SearchBar from "react-native-dynamic-search-bar";
 import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 import { AntDesign } from "@expo/vector-icons";
-
+import { useEffect, useState } from "react";
+//location
+import * as Location from 'expo-location';
 const EvChargerContainer = (props) => {
-    return (
-        <View style={{ flex: 1 }}>
-            <MapView
-                initialRegion={{
-                    latitude: 37.3012,
-                    longitude: 127.0355,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421,
-                }}
-                style={{ flex: 1 }}
-                provider={PROVIDER_GOOGLE}
-                showsUserLocation={true}
-                showsMyLocationButton={true}
-                onMapReady={() => {
-                    // updateMapStyle()
-                }}
-            >
-            </MapView>
-            <SearchBar
-                placeholder="Search here"
-                onPress={() => alert("onPress")}
-                onChangeText={(text) => {
-                    console.log(text)
-                }}
-                onClearPress={() => {
 
-                }}
-            />
+    const [location, setLocation] = useState({
+        latitude: 37.3012,
+        longitude: 127.0355,
+    });
+    const [errorMsg, setErrorMsg] = useState(null);
+
+    // Get current location information 
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;  //권한 거부 시 그대로 종료
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            console.log(location);
+
+            // 실시간으로 위치 변화 감지 (권한 거부 시 아예 동작하지 않음)
+            Location.watchPositionAsync({ accuracy: Location.Accuracy.Balanced, timeInterval: 100, distanceInterval: 1 },
+                position => {
+                    console.log(position.coords);
+                    setLocation({ longitude: position.coords.longitude, latitude: position.coords.latitude });
+                }
+            );
+
+        })();
+
+    }, []);
+
+
+    return (
+        <>
+            <View style={{ flex: 1 }}>
+                <MapView
+                    initialRegion={{
+                        latitude: 37.3012,
+                        longitude: 127.0355,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}
+                    style={{ flex: 1 }}
+                    provider={PROVIDER_GOOGLE}
+                    showsUserLocation={true}
+                    showsMyLocationButton={true}
+                    region={{
+                        latitude: location.latitude,
+                        longitude: location.longitude,
+                        // latitudeDelta: 0.0922,
+                        // longitudeDelta: 0.0421,
+                        latitudeDelta: 0.01,
+                        longitudeDelta: 0.01,
+                    }}
+                    onMapReady={() => {
+                        // updateMapStyle()
+                    }}
+                />
+            </View>
             <Fab
                 renderInPortal={false}
                 shadow={2}
-                size="md"
+                size="sm"
                 style={{ backgroundColor: "#27ae60", }}
-                icon={<Icon color="white" as={AntDesign} name="plus" size="md" />}
+                icon={<Icon color="white" as={AntDesign} name="home" size="sm" />}
                 onPress={() => props.navigation.navigate('Home')}
             />
-        </View>
+        </>
     )
 }
 export default EvChargerContainer;
