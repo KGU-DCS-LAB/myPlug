7a5234cb29d9f8cb174c914dcf855101c5a9f2df
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-06-21 01:33:34 +0900

    스택 효과 적용을 위해 파일 교체

--- app-expo/src/containers/main/MainContainer.js
+++ app-expo/src/containers/main/MainContainer.js
@@ -1,4 +1,5 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+// import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
 import HomeView from "../../views/main/HomeView";
 import EvChargerContainer from "../ev_charger/EvChargerContainer";
 import HomeContainer from "./HomeContainer";
@@ -7,24 +8,30 @@ const MainContainer = () => {
 
     const HomeScreen = ({ navigation }) => {
         return (
-          <HomeContainer navigation={navigation} />
+            <HomeContainer navigation={navigation} />
         );
     }
 
     const EvChargerScreen = ({ navigation }) => {
         return (
-          <EvChargerContainer navigation={navigation} />
+            <EvChargerContainer navigation={navigation} />
         );
     }
-    
-    const Stack = createNativeStackNavigator();
+
+    const Stack = createStackNavigator();
     return (
         <Stack.Navigator
             initialRouteName="Home"
             screenOptions={{ headerShown: false }}
         >
             <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="EvCharger" component={EvChargerScreen} />
+            <Stack.Screen
+                name="EvCharger"
+                component={EvChargerScreen}
+                options={{
+                    cardStyleInterpolator: CardStyleInterpolators.forVerticalIOS,
+                }}
+            />
         </Stack.Navigator>
     )
 }
