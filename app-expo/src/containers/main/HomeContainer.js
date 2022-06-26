83df7a74d05a2f02de34341745dd685e74101f2c
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-06-26 19:16:32 +0900

    메인 디자인 예시 추가

--- app-expo/src/containers/main/HomeContainer.js
+++ app-expo/src/containers/main/HomeContainer.js
@@ -1,4 +1,4 @@
-import { Box, HStack, Spacer, Text } from "native-base";
+import { Box, Heading, HStack, Spacer, Text } from "native-base";
 import { Dimensions, ScrollView, StyleSheet, TouchableOpacity } from "react-native";
 import { MaterialIcons } from '@expo/vector-icons';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
@@ -6,40 +6,89 @@ import PressableButton from "../../components/common/PressableButton";
 
 const HomeContainer = (props) => {
     const windowWidth = Dimensions.get('window').width;
-    const numOfCol = 2;
+    const colNum2 = 2;
+    const colNum3 = 3;
     return (
-        <Box px={2} flex="1">
-            <ScrollView>
-                <Box py={1}/>
-                <HStack justifyContent="center">
-                    <PressableButton
-                        numOfCol={numOfCol}
-                        width={windowWidth/numOfCol*0.9}
-                        height={windowWidth/numOfCol*0.9}
-                        onPress={() => props.navigation.navigate('EvCharger')}
-                        title="충전소 지도"
-                    />
-                    <PressableButton
-                        numOfCol={numOfCol}
-                        width={windowWidth/numOfCol*0.9}
-                        height={windowWidth/numOfCol*0.9}
-                        onPress={() => console.log('ㅇㅇ')}
-                        title="충전 기록하기"
-                    />
-                </HStack>
-                <HStack justifyContent="center">
-                    <PressableButton
-                        numOfCol={numOfCol}
-                        width={windowWidth*0.92}
-                        height={windowWidth/numOfCol*0.9}
-                        onPress={() => console.log('ㅇㅇ')}
-                        title="인근 충전소 찾기"
-                    />
-                </HStack>
-                <Box py={1}/>
-            </ScrollView>
-        </Box>
+        <>
+            <Heading style={styles.heading}>나만의 충전소</Heading>
+            <Box px={2} flex="1">
+                <ScrollView>
+                    <Box py={1} />
+                    <HStack justifyContent="center">
+                        <PressableButton
+                            numOfCol={colNum2}
+                            width={windowWidth / colNum2 * 0.9}
+                            height={windowWidth / colNum2 * 0.9}
+                            onPress={() => props.navigation.navigate('EvCharger')}
+                            title="충전소 지도"
+                        />
+                        <PressableButton
+                            numOfCol={colNum2}
+                            width={windowWidth / colNum2 * 0.9}
+                            height={windowWidth / colNum2 * 0.9}
+                            onPress={() => console.log('ㅇㅇ')}
+                            title="충전 기록하기"
+                        />
+                    </HStack>
+                    <HStack justifyContent="center">
+                        <PressableButton
+                            numOfCol={colNum2}
+                            width={windowWidth * 0.92}
+                            height={windowWidth / colNum2 * 0.7}
+                            onPress={() => console.log('ㅇㅇ')}
+                            title="인근 충전소 찾기"
+                        />
+                    </HStack>
+                    <HStack justifyContent="center">
+                        <PressableButton
+                            numOfCol={colNum3}
+                            width={windowWidth / colNum3 * 0.875}
+                            height={windowWidth / colNum3 * 0.9}
+                            onPress={() => console.log('ㅇㅇ')}
+                            // title="충전 기록하기"
+                        />
+                        <PressableButton
+                            numOfCol={colNum3}
+                            width={windowWidth / colNum3 * 0.875}
+                            height={windowWidth / colNum3 * 0.9}
+                            onPress={() => console.log('ㅇㅇ')}
+                            // title="충전 기록하기"
+                        />
+                        <PressableButton
+                            numOfCol={colNum3}
+                            width={windowWidth / colNum3 * 0.875}
+                            height={windowWidth / colNum3 * 0.9}
+                            onPress={() => console.log('ㅇㅇ')}
+                            // title="충전 기록하기"
+                        />
+                    </HStack>
+                    <HStack justifyContent="center">
+                        <PressableButton
+                            numOfCol={colNum2}
+                            width={windowWidth / colNum2 * 0.9}
+                            height={windowWidth / colNum2 * 0.9}
+                            onPress={() => console.log('ㅇㅇ')}
+                        />
+                        <PressableButton
+                            numOfCol={colNum2}
+                            width={windowWidth / colNum2 * 0.9}
+                            height={windowWidth / colNum2 * 0.9}
+                            onPress={() => console.log('ㅇㅇ')}
+                        />
+                    </HStack>
+                    <Box py={1} />
+                </ScrollView>
+            </Box>
+        </>
     )
 }
 
 export default HomeContainer;
+
+const styles = StyleSheet.create({
+    heading: {
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        backgroundColor: '#fff',
+    },
+});
