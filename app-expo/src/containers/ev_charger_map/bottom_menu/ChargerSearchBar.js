668825e0a99c77a97c31e5f46f0a077bc85ee59b
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-06-26 22:31:20 +0900

    검색창 디자인 푸쉬

--- app-expo/src/containers/ev_charger_map/bottom_menu/ChargerSearchBar.js
+++ app-expo/src/containers/ev_charger_map/bottom_menu/ChargerSearchBar.js
@@ -1,19 +1,40 @@
+import { Box, Text } from "native-base";
+import { useState } from "react";
 import SearchBar from "react-native-dynamic-search-bar";
 
 const ChargerSearchBar = (props) => {
+
+    const [text, setText] = useState('');
+    const filterList = (text) => {
+        const textData = text.toLowerCase();
+        setText(textData);
+    }
+
     return (
         <>
+            {
+                text.length > 0
+                &&
+                <Box
+                    style={{ width: 250, position: 'absolute', bottom: 40 }}
+                    bg="white"
+                    maxH={300}
+                    minH={300}
+                >
+                    <Text>ㅇㅇ</Text>
+                    <Text>ㅇㅇ</Text>
+                    <Text>ㅇㅇ</Text>
+                </Box>
+            }
+
             <SearchBar
                 style={{ width: 250 }}
                 placeholder="충전소 검색하기"
                 onPress={() => alert("onPress")}
-            // onChangeText={(text) => {
-            //     console.log(text)
-            //     filterList(text);
-            // }}
-            // onClearPress={() => {
-            //     filterList("");
-            // }}
+                onChangeText={(text) => {
+                    console.log(text)
+                    filterList(text);
+                }}
             />
         </>
     )
