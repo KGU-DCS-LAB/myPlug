63e3f9072ce4233c6602f1537f608c2efcb6f57a
seonae <sunellie55@gmail.com>
2022-07-09 23:14:14 +0900

    즐겨찾기 추가 구현

--- app-expo/src/components/ev_charger_map/FindFavorites.js
+++ app-expo/src/components/ev_charger_map/FindFavorites.js
@@ -9,44 +9,81 @@ import { useIsFocused } from '@react-navigation/native';
 const FindFavorites = ({user_id, statNm}) => {
     const [star, setStar] = useState('star-border');
     const isFocused = useIsFocused();
+    const [firstRecord, setFirstRecord] = useState(true);
     const [favorites, setFavorites] = useState([]);
+    const [bookmarked, setBookmarked] = useState(false);
+    const [currentStatNm, setCurrentStatNm] = useState('');
     
     useEffect(() => {
         getFavorites();
-    }, [isFocused]);
+    }, [isFocused, statNm]);
 
     const getFavorites = () => {
         let result = []
         axios.post(config.ip + ':5000/favoritesRouter/findOwn', {
             user_id: user_id
         }).then((response) => {
-            if (response.data.length > 0) {
-                response.data.forEach((item) => {
-                    result.push(item);
-                });
-            } 
-            setFavorites(result);
-        }).catch(function (error) {
-            console.log(error);
-        })
+            console.log(response.data);
+            if (response.data.length == 0) {
+                setFirstRecord(true)
+              } else {
+                result.push(response.data[0].station)
+                setFirstRecord(false);
+                setFavorites(result);
+                for(let i=0; i<result[0].length; i++){
+                    if(result[0][i].statNm == statNm){
+                        setStar('star');
+                        break;
+                    }
+                }
+                console.log(result[0][0]);
+              }
+            }).catch(function (error) {
+              console.log(error);
+              setFirstRecord(true)
+            })
     }
 
     const addToFavorites = () => {
-        axios.post(config.ip + ':5000/favoritesRouter/save', {
-            data: {
-                user_id: user_id,
-                station: {statNm: statNm},
-            }
-        }).then((response) => {
-            if (response.data.status === 'success') {
-                setStar('star');
-                console.log('즐겨찾기 성공');
+        setCurrentStatNm(statNm);
+        if(star == 'star'){
+            //즐겨찾기 취소
+            setStar('star-border');
+        } else{
+            if(firstRecord){
+                axios.post(config.ip + ':5000/favoritesRouter/save', {
+                    data: {
+                        user_id: user_id,
+                        station: {statNm: statNm},
+                    }
+                }).then((response) => {
+                    if (response.data.status === 'success') {
+                        setStar('star');
+                        console.log('즐겨찾기 성공');
+                    } else {
+                        alert('즐겨찾기 실패');
+                    }
+                }).catch(function (error) {
+                    console.log(error);
+                })
             } else {
-                alert('즐겨찾기 실패');
+                axios.post(config.ip + ':5000/favoritesRouter/saveMore', {
+                    data: {
+                        user_id: user_id,
+                        statNm: statNm,
+                    }
+                }).then((response) => {
+                    if (response.data.status === 'success') {
+                        setStar('star');
+                        console.log('즐겨찾기 성공');
+                    } else {
+                        alert('즐겨찾기 실패');
+                    }
+                }).catch(function (error) {
+                    console.log(error);
+                })
             }
-        }).catch(function (error) {
-            console.log(error);
-        })
+        }
     }
 
     return (
