95895d3c31865b01bde32d08ef8e5d1130638a2f
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-07-23 16:07:10 +0900

    java 1.8 -> 17

--- stand-alone/station-data-generator/src/v2/DataManager.java
+++ stand-alone/station-data-generator/src/v2/DataManager.java
@@ -13,33 +13,33 @@ import java.util.Scanner;
 
 /**
  * API를 활용한 전기차 충전소 데이터 조회
- * 
+ * <p>
  * 한번에 하나씩 작업이 가능하며
  * 1. 데이터를 수신받고
  * 2. 수신 받은 데이터를 정제하여
  * 3. 정제된 데이터를 DB로 저장
  * 이런 순서로 작업을 해야한다.
- * 
+ * <p>
  * 지금은 일회성이지만, 앞으로는 자동화가 필요할 것으로 보임
- * */
+ */
 
 
 public class DataManager {
-    public static DataManager dataManager = new DataManager();;
+    public static DataManager dataManager = new DataManager();
     public static ArrayList<KecoChargerInfoDTO> chargerInfoList = new ArrayList<KecoChargerInfoDTO>();
     public static ConsoleColor cc = new ConsoleColor();
-    long startTime, finishTime ,elapsedTime;
+    long startTime, finishTime, elapsedTime;
 
     public void run() throws IOException {
 
         Scanner scan = new Scanner(System.in);
         while (true) {
-            System.out.println("--------Version2--------");
+            System.out.println("--------Version2 (Java17)-------");
             System.out.println("[메뉴] 숫자를 입력 후 엔터를 누르세요. (순서대로 작업해야 합니다.)");
             System.out.println("0. 프로그램 종료");
-            System.out.println("1. API 서버로 부터 데이터 수신 후 클라우드로 저장하기");
-            System.out.println("2. 수신받은 데이터 분석하기");
-            cc.print("danger","8507. 고급모드");
+            System.out.println("1. API 서버로 부터 데이터 수신 후 클라우드로 저장하기 후 데이터 통계 분석하기");
+//            System.out.println("2. 수신받은 데이터 분석하기");
+            cc.print("danger", "8507. 고급모드");
             System.out.println("------------------------");
             int mode = scan.nextInt();
             switch (mode) {
@@ -54,26 +54,27 @@ public class DataManager {
                     rc2.start();
                     finishTime = System.currentTimeMillis();
                     elapsedTime = finishTime - startTime;
-                    cc.print("secondary", "수행에 걸린 시간 : "+elapsedTime+"ms\n");
-                    if(chargerInfoList.size()>0){
+                    cc.print("secondary", "수행에 걸린 시간 : " + elapsedTime + "ms\n");
+                    if (chargerInfoList.size() > 0) {
                         Saver sc = Saver.getInstance();
                         sc.start();
-//                        System.out.println("안전을 위해 프로그램을 종료합니다.");
-//                        System.exit(0);
-                    }
-                    else{
-                        cc.print("danger","외부로부터 수신한 데이터가 없습니다.");
-                    }
-                    break;
-                case 2:
-                    if(chargerInfoList.size()>0){
                         Analyzer analyzer = Analyzer.getInstance();
                         analyzer.start();
-                    }
-                    else{
-                        cc.print("danger","외부로부터 수신한 데이터가 없습니다.");
+                        System.out.println("안전을 위해 프로그램을 종료합니다.");
+                        System.exit(0);
+                    } else {
+                        cc.print("danger", "외부로부터 수신한 데이터가 없습니다.");
                     }
                     break;
+//                case 2:
+//                    if(chargerInfoList.size()>0){
+//                        Analyzer analyzer = Analyzer.getInstance();
+//                        analyzer.start();
+//                    }
+//                    else{
+//                        cc.print("danger","외부로부터 수신한 데이터가 없습니다.");
+//                    }
+//                    break;
                 case 8507:
                     ManagerController mc = ManagerController.getInstance();
                     mc.start();
