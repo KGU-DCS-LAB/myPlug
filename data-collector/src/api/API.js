f9a0d6e4b8b2e700c662f7bb8f9d5aa70ff263dd
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-08-14 03:27:16 +0900

    코드 수신 및 기존의 페이징 자동화 기능 이식

--- data-collector/src/api/API.js
+++ data-collector/src/api/API.js
@@ -4,9 +4,13 @@ export const getChargerInfo = async (url) => {
     try {
         const response = await axios.get(url)
         // console.log("response >>", response.data)
-        return response.data
+        // return response.data
+        const header = response.data.header[0];
+        const item = response.data.items[0].item;
+        return {header, item}
     } catch (err) {
         console.log("Error >>", err);
-        return []
+        const temp = null;
+        return {temp, temp}
     }
 }
--- data-collector/src/controller/receiver/receiver.js
+++ data-collector/src/controller/receiver/receiver.js
@@ -1,27 +1,4 @@
-/* Javascript 샘플 코드 */
-
-
-// var xhr = new XMLHttpRequest();
-// var url = 'http://apis.data.go.kr/B552584/EvCharger/getChargerStatus'; /*URL*/
-// var queryParams = '?' + encodeURIComponent('serviceKey') + '='+'서비스키'; /*Service Key*/
-// queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /**/
-// queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /**/
-// queryParams += '&' + encodeURIComponent('period') + '=' + encodeURIComponent('5'); /**/
-// queryParams += '&' + encodeURIComponent('zcode') + '=' + encodeURIComponent('11'); /**/
-// xhr.open('GET', url + queryParams);
-// xhr.onreadystatechange = function () {
-//     if (this.readyState == 4) {
-//         alert('Status: '+this.status+'nHeaders: '+JSON.stringify(this.getAllResponseHeaders())+'nBody: '+this.responseText);
-//     }
-// };
-
-// xhr.send('');
-
 import * as API from '../../api/API.js';
-
-export const init = async () => {
-    zcodes.map((z) => getChargerInfoByZcode(z.code))
-}
 
 const zcodes = [
     { 'code': 11, 'region': '서울특별시' },
@@ -31,7 +8,7 @@ const zcodes = [
     // { 'code': 29, 'region': '광주광역시' },
     // { 'code': 30, 'region': '대전광역시' },
     // { 'code': 31, 'region': '울산광역시' },
-    // { 'code': 41, 'region': '경기도' },
+    { 'code': 41, 'region': '경기도' },
     // { 'code': 42, 'region': '강원도' },
     // { 'code': 43, 'region': '충청북도' },
     // { 'code': 44, 'region': '충청남도' },
@@ -42,22 +19,37 @@ const zcodes = [
     // { 'code': 50, 'region': '제주도' },
 ]
 
+let data = []
+
+export const init = async () => {
+    await Promise.all(zcodes.map((z) => getChargerInfoByZcode(z.code))); //순서를 지켜주기 위함
+    // console.log(data);
+    console.log('current data : '+data.length);
+}
+
 const getChargerInfoByZcode = async (zcode) => {
     console.log(zcode)
     let totalCount = 10000; // 실행 시 업데이트 되는 부분
     let numOfRows = 9999;
     let page = 1;	// 페이지 초기값
     let maxPage = 2; // 추후 수정 예정
-    // while (true) {
-    //     if (page > maxPage) {
-    //         break;
-    //     }
+    while (true) {
+        if (page > maxPage) {
+            break;
+        }
         console.log(">>KECO 전기자동차 충전소 정보 수집 요청(" + page + "페이지)");
         let url = "http://apis.data.go.kr/B552584/EvCharger/getChargerInfo?serviceKey=dg8oHXO5d9HkXM00ye%2Bvpwk1w16hxVZxN9UGvCFKA8kXtHQhTb6CJebWA2WZdMszfK%2B9HgoiqEYCB%2Bze2hFWMQ%3D%3D";
         url += "&pageNo=" + page; // 페이지 번호 : 페이지 번호
         url += "&numOfRows=" + numOfRows; //한 페이지 결과 수 : 한 페이지 결과 수 (최소 10, 최대 9999)
         url += "&zcode=" + zcode; //지역구분 코드 시도 코드 (행정구역코드 앞 2자리)
         console.log("요청 url : " + url);
-        console.log(await API.getChargerInfo(url));
-    // }
+        const {header, item} = await API.getChargerInfo(url);
+        data.push(...item);
+        totalCount=header.totalCount;
+        maxPage=parseInt(totalCount/numOfRows)+1;
+        console.log("page ("+page+"/"+maxPage+")");
+        console.log("count ("+ (page*numOfRows>totalCount?totalCount:page*numOfRows) +"/"+totalCount+")");
+        console.log();
+        page++;
+    }
 }
