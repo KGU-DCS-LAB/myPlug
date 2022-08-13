06960a43de235c6468ce4289103f76a696deeb44
soyoung125 <sypark041754@gmail.com>
2022-08-13 15:44:58 +0900

    필터 모달 충전기 타입 버튼 변경

--- app-expo/src/components/ev_charger_map/modals/filterModal/SelectChgerType.js
+++ app-expo/src/components/ev_charger_map/modals/filterModal/SelectChgerType.js
@@ -2,24 +2,61 @@ import { Box, Button, Heading, HStack, Text, View } from "native-base";
 import { StyleSheet } from "react-native";
 
 const SelectChgerType = (props) => {
-    const chgerType = 'chgerType';
+    const chgerType = [
+        {
+            _id: '01',
+            label: 'DC차데모'
+        },
+        {
+            _id: '02',
+            label: 'AC완속'
+        },
+        {
+            _id: '03',
+            label: 'DC차데모+AC3상'
+        },
+        {
+            _id: '04',
+            label: 'DC콤보'
+        },
+        {
+            _id: '05',
+            label: 'DC차데모+DC콤보'
+        },
+        {
+            _id: '06',
+            label: 'DC차데모+AC3상+DC콤보'
+        },
+        {
+            _id: '07',
+            label: 'AC3상'
+        },
+    ]
+    const type = 'chgerType';
     return (
         <>
             <Box my={1}>
                 <Heading>충전기 종류</Heading>
             </Box>
-            <HStack>
-                {props.chgerType.map((item) =>
-                    props.selectedType[chgerType].findIndex((type) => type === item._id) !== -1 ?
-                        <Button key={item._id} onPress={() => props.cancelSelect(chgerType, item._id)} style={styles.selectedButtom}>{item._id}</Button>
+            <View style={styles.container}>
+                {chgerType.map((item) =>
+                    props.selectedType[type].findIndex((type) => type === item._id) !== -1 ?
+                        <Button key={item._id} onPress={() => props.cancelSelect(type, item._id)} style={styles.selectedButtom}>{item.label}</Button>
                         :
-                        <Button style={styles.basicButton} key={item._id} onPress={() => props.selectType(chgerType, item._id)}>{item._id}</Button>
+                        <Button style={styles.basicButton} key={item._id} onPress={() => props.selectType(type, item._id)}>{item.label}</Button>
                 )}
-            </HStack></>
+            </View>
+        </>
     )
 }
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexWrap: "wrap",
+        flexDirection: "row",
+        width: "95%"
+    },
     basicButton: {
         margin: 2,
         backgroundColor: "grey"
