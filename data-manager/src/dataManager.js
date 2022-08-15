b483fff44770cab4345b8bef3e049f7c72e3e728
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-08-15 13:14:48 +0900

    mongoose 적용중

--- data-manager/src/dataManager.js
+++ data-manager/src/dataManager.js
@@ -1,5 +1,13 @@
-import * as receiver from './controller/receiver/receiver.js'
+import * as receiver from './controller/manager/receiver.js'
+// Using ES6 imports
+import mongoose from 'mongoose';
+
 export const run = async () => {
+
+    await mongoose.connect(`mongodb+srv://gabrielyoon7:0000@gabrielyoon7.aq0fu.mongodb.net/myplug?retryWrites=true&w=majority`, {})
+    .then(() => console.log('MongoDB Connected!!'))
+    .catch(err => console.log(err))
+
     await receiver.init();
 }
 
