5127d9bf2725f1df2f2564214e86c34a075d9844
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-08-28 18:25:35 +0900

    일정 관리에서 버튼 눌렀을 때 색상이 변하는 기능 추가

--- app-expo/src/containers/schedule/TimeTable.js
+++ app-expo/src/containers/schedule/TimeTable.js
@@ -1,23 +1,26 @@
-import { Center, HStack, ScrollView, Text, VStack } from "native-base"
-import React from "react";
+import { Center, HStack, Pressable, ScrollView, Text, VStack } from "native-base"
+import React, { useState } from "react";
 
 const TimeTable = (props) => {
 
+
+    const [table, setTable] = useState({
+        "mon": { "0": 0, "1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8": 0, "9": 0, "10": 0, "11": 0, "12": 0, "13": 0, "14": 0, "15": 0, "16": 0, "17": 0, "18": 0, "19": 0, "20": 0, "21": 0, "22": 0, "23": 0, },
+        "tue": { "0": 0, "1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8": 0, "9": 0, "10": 0, "11": 0, "12": 0, "13": 0, "14": 0, "15": 0, "16": 0, "17": 0, "18": 0, "19": 0, "20": 0, "21": 0, "22": 0, "23": 0, },
+        "wed": { "0": 0, "1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8": 0, "9": 0, "10": 0, "11": 0, "12": 0, "13": 0, "14": 0, "15": 0, "16": 0, "17": 0, "18": 0, "19": 0, "20": 0, "21": 0, "22": 0, "23": 0, },
+        "thu": { "0": 0, "1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8": 0, "9": 0, "10": 0, "11": 0, "12": 0, "13": 0, "14": 0, "15": 0, "16": 0, "17": 0, "18": 0, "19": 0, "20": 0, "21": 0, "22": 0, "23": 0, },
+        "fri": { "0": 0, "1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8": 0, "9": 0, "10": 0, "11": 0, "12": 0, "13": 0, "14": 0, "15": 0, "16": 0, "17": 0, "18": 0, "19": 0, "20": 0, "21": 0, "22": 0, "23": 0, },
+        "sat": { "0": 0, "1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8": 0, "9": 0, "10": 0, "11": 0, "12": 0, "13": 0, "14": 0, "15": 0, "16": 0, "17": 0, "18": 0, "19": 0, "20": 0, "21": 0, "22": 0, "23": 0, },
+        "sun": { "0": 0, "1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0, "8": 0, "9": 0, "10": 0, "11": 0, "12": 0, "13": 0, "14": 0, "15": 0, "16": 0, "17": 0, "18": 0, "19": 0, "20": 0, "21": 0, "22": 0, "23": 0, }
+    })
+
     const day = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
-    const koreanDay = {
-        'mon':'월',
-        'tue':'화',
-        'wed':'수', 
-        'thu':'목',
-        'fri':'금', 
-        'sat':'토', 
-        'sun':'일'
-    };
+    const koreanDay = { 'mon': '월', 'tue': '화', 'wed': '수', 'thu': '목', 'fri': '금', 'sat': '토', 'sun': '일' };
 
     const TimeHeader = () => {
         let render = [];
         for (let index = 0; index < 24; index++) {
-            render.push(<Center h="10" w="10" key={index}><Text>{index}</Text></Center>);
+            render.push(<Center h="10" w="10" key={index}><Text fontSize={22}>{index}</Text></Center>);
         }
         return (
             <Center>
@@ -30,26 +33,37 @@ const TimeTable = (props) => {
     }
 
     const TimeLine = (day) => {
+        const modifyTimeTableJSON = (day, i, value) => {
+            var temp = { ...table };
+            temp[day][i] = value;
+            return temp;
+        }
         let render = [];
         for (let i = 0; i < 24; i++) {
-            render.push(<Center h="10" w="10" bg="primary.300" rounded="md" shadow={3} key={i} />)
+            if (table[day.day][i] === 0) {
+                render.push(<Pressable key={i} onPress={() => setTable(modifyTimeTableJSON(day.day, i, 1))}><Center h="10" w="10" bg="primary.300" rounded="md" shadow={3} /></Pressable>)
+            }
+            else {
+                render.push(<Pressable key={i} onPress={() => setTable(modifyTimeTableJSON(day.day, i, 0))}><Center h="10" w="10" bg="primary.700" rounded="md" shadow={3} /></Pressable>)
+            }
         }
         return (
             <Center>
                 <HStack space={1} justifyContent="center">
-                    <Text h="10" w="10" >{koreanDay[day.day]}</Text>
+                    <Text h="10" w="10" fontSize={22}>{koreanDay[day.day]}</Text>
                     {render}
                 </HStack>
             </Center>
         )
     }
+    const MemoizedTimeLine = React.memo(TimeLine);
 
     return (
         <ScrollView horizontal={true}>
             <VStack space={1} alignItems="center" m={4}>
                 <TimeHeader />
                 {day.map((d) => (
-                    <TimeLine key={d} day={d}/>
+                    <MemoizedTimeLine key={d} day={d} />
                 ))}
             </VStack>
         </ScrollView>
